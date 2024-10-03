d0b5eed2e93c73945448b94de4bf9d408aba8b45
Luxzzz0101 <tesakun6786@gmail.com>
2024-10-03 23:48:52 +0700

    aws.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'a00d9240-9d38-47fc-8afb-cbe8a01159d6';
 
-const พร็อกซีไอพีs = ['178.128.91.137'];
+const พร็อกซีไอพีs = ['18.141.204.88'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
